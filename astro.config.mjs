a7a5be8e3403929a8dcae37acec5da363c041fe0
kyeshmz <kyeshimizu@gmail.com>
2024-09-12 11:15:22 -0400

    fix base tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://fab.cba.mit.edu/classes/863.24/people/KyeShimizu/",
-  base: "/classes/863.24/people/KyeShimizu/",
-  // trailingSlash: "never",
+  base: "/classes/863.24/people/KyeShimizu",
+  trailingSlash: "always",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
