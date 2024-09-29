e0775a7de47b1419276d081756fcb89ff5e9d539
kyeshmz <kyeshimizu@gmail.com>
2024-09-29 16:48:39 -0400

    fix to use dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -71,7 +71,7 @@ export default defineConfig({
   ],
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
-  outDir: "public",
+  outDir: "dist",
 
   // The folder name Astro uses for static files (`public`) is already reserved
   // for the build output. So in deviation from the defaults we're using a folder
