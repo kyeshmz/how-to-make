83d588c3c1ab75fad318a3b30e75c92445a95e58
kyeshmz <kyeshimizu@gmail.com>
2024-11-06 13:07:31 -0500

    try pushing without files

--- astro.config.mjs
+++ astro.config.mjs
@@ -72,9 +72,4 @@ export default defineConfig({
   // GitLab Pages requires exposed files to be located in a folder called "public".
   // So we're instructing Astro to put the static build output in a folder of that name.
   outDir: "dist",
-
-  // The folder name Astro uses for static files (`public`) is already reserved
-  // for the build output. So in deviation from the defaults we're using a folder
-  // called `static` instead.
-  publicDir: "static",
 });
